2d8b8ef52a55cf5214d9660bd5fe253d0d14e3f0
Joel Vitelli <jvitelli92@gmail.com>
2022-11-09 13:39:30 -0300

    First commit

---
+++ src/test/java/playwrigth/Prueba.java
@@ -0,0 +1,11 @@
+package playwrigth;
+
+import com.microsoft.playwright.*;
+import org.testng.annotations.Test;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+@Test
+public class Prueba {
+
+
+}
